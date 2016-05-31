fd53f8a67c667224b272265ff87aedc29cb3aa3a
lewadik <lewadik@gmail.com>
2016-05-31 17:01:55 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ if [ ! -f /.glassfish_admin_password_changed ]; then
 fi
 echo "=> Restarting Glassfish server"
 asadmin stop-domain
-asadmin restore-domain --backupdir /tmp domain1
+asadmin restore-domain  domain1 --filename /tmp/domain1/domain1_2016_05_31_v00001.zip --force
 echo "=> Starting and running Glassfish server"
 #mv /*.war /opt/glassfish3/glassfish/domains/domain1/autodeploy/
 #rm -rf /opt/glassfish3/glassfish/domains/domain1/config/

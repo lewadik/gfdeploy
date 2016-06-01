2e20c13aeb36fadc884bbb932cc6fc7a80b0b83b
lewadik <lewadik@gmail.com>
2016-06-01 15:00:37 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ set -e
 
 asadmin restore-domain --filename /tmp/domain1_2016_05_31_v00001.zip --force domain1
 asadmin start-domain
+echo "=> Starting restore script"
 /restore.sh
 asadmin stop-domain
 echo "=> Starting and running Glassfish server"

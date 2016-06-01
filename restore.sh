1b10825e876d716ea43c55e9c245bc42187d0907
lewadik <lewadik@gmail.com>
2016-06-01 15:08:19 +0300

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -10,6 +10,9 @@ agent.das.host=localhost
 agent.das.isSecure=false
 EOT
 
-cd /opt/glassfish3/glassfish/nodes/localhost-domain1/ && mkdir instance1-gs2c/ && mkdir instance1-userservice/ && mkdir instance1-dataexportapi/ && mkdir instance1-inwardservice/ && mkdir instance1-inward/ && mkdir instance1-system-service/ && mkdir instance1-cis/ && mkdir instance1-fin/ && mkdir instance1-cashier/ && instance1-gms && mkdir instance1-bingoengine/
+cd /opt/glassfish3/glassfish/nodes/localhost-domain1/ && mkdir instance1-gs2c/ && mkdir instance1-userservice/ && \
+mkdir instance1-dataexportapi/ && mkdir instance1-inwardservice/ && mkdir instance1-inward/ && \
+mkdir instance1-system-service/ && mkdir instance1-cis/ && mkdir instance1-fin/ && mkdir instance1-cashier/ && \
+mkdir instance1-gms && mkdir instance1-bingoengine/
 
 asadmin --user admin --passwordfile glassfish.passwords start-local-instance --node localhost-domain1 --sync full instance1-userservice

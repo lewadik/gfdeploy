dce681e4491705581306b3888053b79e7a5d528e
lewadik <lewadik@gmail.com>
2016-06-01 23:33:23 +0300

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -14,17 +14,19 @@ mkdir instance1-dataexportapi/ && mkdir instance1-inwardservice/ && mkdir instan
 mkdir instance1-system-service/ && mkdir instance1-cis/ && mkdir instance1-fin/ && mkdir instance1-cashier/ && \
 mkdir instance1-gms && mkdir instance1-bingoengine/
 
-asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster gs2c-cluster
-asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster userservice-cluster
-asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster dataexportapi-cluster
-asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster inwardservice-cluster
-asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster inward-cluster
-asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster system-service-cluster
-asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster casino-integration-service-cluster
-asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster financial-service-cluster
-asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster cashier-cluster
-asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster game-management-service-cluster
-asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster bingo-engine-cluster
+asadmin --interactive=false --user admin --passwordfile /glassfish.passwords start-local-instance --node localhost-domain1 --sync full instance1-userservice
+asadmin --interactive=false --user admin --passwordfile /glassfish.passwords start-cluster userservice-cluster
+#asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster gs2c-cluster
+#asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster userservice-cluster
+#asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster dataexportapi-cluster
+#asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster inwardservice-cluster
+#asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster inward-cluster
+#asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster system-service-cluster
+#asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster casino-integration-service-cluster
+#asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster financial-service-cluster
+#asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster cashier-cluster
+#asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster game-management-service-cluster
+#asadmin --user admin --passwordfile /glassfish.passwords --interactive=false start-cluster bingo-engine-cluster
 #expect -c 'spawn asadmin --user admin --passwordfile /glassfish.passwords start-local-instance --node localhost-domain1 --sync full instance1-userservice;
 #expect "Do you trust the above certificate*" ;
 #send "y\r";

56244d574da60cc27533b204600a01449f0b0584
lewadik <lewadik@gmail.com>
2016-06-01 14:57:54 +0300

    Create restore.sh

---
+++ restore.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p /opt/glassfish3/glassfish/nodes/localhost-domain1/agent/config
+cat << EOT > /opt/glassfish3/glassfish/nodes/localhost-domain1/agent/config/das.properties
+agent.das.protocol=http
+agent.das.port=4848
+agent.das.host=localhost
+agent.das.isSecure=false
+EOT
+
+cd /opt/glassfish3/glassfish/nodes/localhost-domain1/ && mkdir instance1-gs2c/ && mkdir instance1-userservice/ && mkdir instance1-dataexportapi/ && mkdir instance1-inwardservice/ && mkdir instance1-inward/ && mkdir instance1-system-service/ && mkdir instance1-cis/ && mkdir instance1-fin/ && mkdir instance1-cashier/ && instance1-gms && mkdir instance1-bingoengine/
+
+asadmin --user admin --passwordfile glassfish.passwords start-local-instance --node localhost-domain1 --sync full instance1-userservice
